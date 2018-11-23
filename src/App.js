832ff950d02b5f65de86380ead91b4480963aeff
Andy Hay <superandystar@gmail.com>
2018-11-23 12:29:23 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from "react"
 import "./App.css"
-import Letter from "@disney/letter"
-import Data from "./data"
 
 class App extends Component {
   render() {
-    return (
-      <div className="letters-container">
-        {Data.map(icon => (
-          <Letter key={icon.path} icon={icon} />
-        ))}
-      </div>
-    )
+    return <div>hello</div>
   }
 }
 
