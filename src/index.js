b02b50e3b9dc4b97c2974c8f54014f8818ba751c
Andy Hay <superandystar@gmail.com>
2018-11-25 08:23:34 -0500

    updating theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "normalize.css"
+import "./fonts.css"
+import "./theme.css"
 import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
