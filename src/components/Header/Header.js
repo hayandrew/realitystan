e76206c53a2b581c18f916258c65ee196b918490
Andy Hay <andy@andyhay.com>
2026-08-29 07:38:03 -0400

    updating header and adding logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+import Image from "next/image";
+
 const Header = () => {
   return (
     <div className="header">
       <div className="header-left">
+        <Image
+          src="/images/logo.png"
+          alt="RealityStan Logo"
+          width={548}
+          height={312}
+          className="header-logo"
+          preload
+        />
         <h1>RealityStan</h1>
       </div>
     </div>
